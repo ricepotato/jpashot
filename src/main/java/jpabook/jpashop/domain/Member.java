de1b37babd86dfdb5a87441bd12e014238da379e
ricepotato <ricepotato40@gmail.com>
2022-05-29 04:56:51 +0900

    domain 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
+    public List<Order> getOrders() {
+        return this.orders;
+    }
+}
