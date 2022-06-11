dcd56b718bc48b79bc90440371f69893dbee1abe
ricepotato <sukjun40@naver.com>
2022-06-12 01:27:32 +0900

    get Members V2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -11,11 +11,48 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    /* Entity 객체를 노출하게 된다.
+    orders 를 뺴고 회원정보만 출력하고 싶은 경우에 @JsonIgnore 로 처리할 수 있다.
+    그러나 다양한 요구사항에 대처가 어렵다.
+    다양한 요구사항이 Entity 객체에 들어가면 프리젠테이션 영역의 요구사항이 Entity 에 포함된다.
+    Entity 변경사항이 Api spec 에 영향을 미치게된다. (장애로 이어진다.)
+    */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 
     /* Entity 객체를 api 에 노출해서는 안된다.
     *  request 받을 때 Entity 객체를 사용해선 안된다. */
@@ -67,4 +104,6 @@ public class MemberApiController {
         @NotEmpty
         private String name;
     }
+
+
 }
