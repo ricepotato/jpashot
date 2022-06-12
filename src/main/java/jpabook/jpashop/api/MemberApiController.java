e289b4c68113078faff6fbdde3a8bcaba9e4380c
ricepotato <sukjun40@naver.com>
2022-06-12 22:20:34 +0900

    simple order v2

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -89,11 +89,9 @@ public class MemberApiController {
         private Long id;
         private String name;
     }
-
     @Data
     static class CreateMemberResponse{
         private Long id;
-
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
