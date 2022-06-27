cd873a35bcdafbea844e3f1cb0923cd7f4bd63cc
ricepotato <sukjun40@naver.com>
2022-06-27 23:25:18 +0900

    orders v5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -64,8 +64,14 @@ public class OrderApiController {
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4(){
         return orderQueryRepository.findOrderQueryDtos();
-
     }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDtoOptimization();
+    }
+
+
 
 
     @Getter
